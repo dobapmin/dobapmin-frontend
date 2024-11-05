294fe6f74a2ecd44aeece03ec70cf2a0bde1c6e8
goeunP <0young384@gmail.com>
2024-11-05 14:44:20 +0900

    Feat(#1): add header component

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import mainLogo from "../../assets/mainLogo.png";
+import userImg from "../../assets/userImg.png";
+import { HandleLogout } from "../../lib/apis/login";
+
+export default function Header() {
+  return (
+    <div
+      style={{
+        height: "10vh",
+        backgroundColor: "white",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginLeft: "100px",
+        marginRight: "100px",
+      }}
+    >
+      <img src={mainLogo} style={{ objectFit: "cover", height: "100%" }} />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          gap: "30px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: "5px",
+          }}
+        >
+          <img src={userImg} style={{ width: "25px", height: "25px" }} />
+          <div
+            style={{
+              fontFamily: "Jalnan",
+            }}
+          >
+            이유진
+          </div>
+        </div>
+
+        <button
+          style={{
+            backgroundColor: "#002DA7",
+            color: "white",
+            height: "45px",
+            width: "120px",
+            fontFamily: "Jalnan",
+            borderRadius: "15px",
+            border: "none",
+          }}
+          onClick={HandleLogout}
+        >
+          {" "}
+          로그아웃
+        </button>
+      </div>
+    </div>
+  );
+}
