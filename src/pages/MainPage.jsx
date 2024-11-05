5d0beab50acf29042afd70bdd7ada6bbdc0a33d5
ZZZINU <nnjin987@gmail.com>
2024-11-06 08:07:13 +0900

    Feat(#2): Post 버튼 컴포넌트 구현

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import BoardList from '../components/main/boardList/BoardList';
+import PostButton from '../components/main/postButton/PostButton';
 
 export default function MainPage() {
   return (
-    <div>
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '60px' }}>
-        <BoardList title={'내가 참여한 게시글'} />
-        <BoardList title={'밥 게시글'} />
-        <BoardList title={'내기 게시글'} />
-        <BoardList title={'마감된 게시글'} />
-      </div>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '60px',
+        padding: '60px 0px',
+      }}
+    >
+      <BoardList title={'내가 참여한 게시글'} />
+      <BoardList title={'밥 게시글'} />
+      <BoardList title={'내기 게시글'} />
+      <BoardList title={'마감된 게시글'} />
+
+      {/* 게시물 포스트 버튼 */}
+      <PostButton />
     </div>
   );
 }
