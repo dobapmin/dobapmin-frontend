03e8183b54e1e6a40c42693de726e7414d46e0ca
ZZZINU <nnjin987@gmail.com>
2024-11-06 08:42:36 +0900

    Feat(#2): 게시글 리스트 타이틀 아이콘 추가

--- src/components/main/boardTitle/BoardTitle.jsx
+++ src/components/main/boardTitle/BoardTitle.jsx
@@ -1,5 +1,15 @@
 import './style.css';
+import PinIcon from '../../../assets/pin.png';
 
 export default function BoardTitle(props) {
-  return <div className="board-title">{props.title}</div>;
+  return (
+    <div className="board-title">
+      {props.title === '내가 참여한 게시글' ? (
+        <>
+          <img className="pin-icon" src={PinIcon} />
+        </>
+      ) : null}
+      {props.title}
+    </div>
+  );
 }
