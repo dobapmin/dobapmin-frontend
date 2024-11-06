4094f46c19bad267e423e24f724d175856bb2cc3
goeunP <0young384@gmail.com>
2024-11-06 16:35:48 +0900

    Feat(#11): fix post login

--- src/lib/apis/login.js
+++ src/lib/apis/login.js
@@ -6,12 +6,12 @@ export const postLogin = async (nickname) => {
   console.log("name111111", nickname);
   try {
     console.log(nickname);
-    const res = await axios.post("http://localhost:3000/", {
-      nickname: nickname,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    // const res = await axios.post("http://localhost:3000/", {
+    //   nickname: nickname,
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // });
     return await res.json();
   } catch (err) {
     console.log(err);
