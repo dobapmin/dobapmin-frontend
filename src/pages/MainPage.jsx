c6b18ccf89247c6a7603f63a6f362eaa26d6d116
ZZZINU <nnjin987@gmail.com>
2024-11-08 01:10:33 +0900

    Feat(#19): 마감된 게시글만 추출

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -7,13 +7,22 @@ import { fetchMainData } from '../lib/apis/main';
 export default function MainPage() {
   const [boardList, setBoardList] = useState([]);
   const [gameBoardList, setGameBoardList] = useState([]);
+  const [endList, setEndList] = useState([]);
 
   useEffect(() => {
     fetchMainData().then((data) => {
       setBoardList(data.boards);
       setGameBoardList(data.gameBoards);
+
+      // 마감된 게시글만 추출
+      const endedBoards = data.boards.filter((board) => board.isEnd);
+      const endedGameBoards = data.gameBoards.filter(
+        (gameBoard) => gameBoard.isEnd
+      );
+      setEndList([...endedBoards, ...endedGameBoards]);
     });
   }, []);
+
   const dummyData = [
     {
       category: '게임',
@@ -129,7 +138,7 @@ export default function MainPage() {
       <BoardList title={'내가 참여한 게시글'} data={dummyData} />
       <BoardList title={'밥 게시글'} data={boardList} />
       <BoardList title={'내기 게시글'} data={gameBoardList} />
-      <BoardList title={'마감된 게시글'} />
+      <BoardList title={'마감된 게시글'} data={endList} />
 
       {/* 게시물 포스트 버튼 */}
       <PostButton />
