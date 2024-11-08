f64652f3b51b5d52b7b2efd5d47c4ebcbb901292
ZZZINU <nnjin987@gmail.com>
2024-11-08 09:08:21 +0900

    Fix(#19): 배경색 크기 조정

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-import Header from "./Header";
-import { Outlet } from "react-router-dom";
+import React from 'react';
+import Header from './Header';
+import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
-    <div>
+    <div style={{ backgroundColor: '#F7F8FA', minHeight: '100vh' }}>
       <Header />
       <div
-        style={{ backgroundColor: '#F7F8FA', height: '100vh', width: '100%', backgroundAttachment: 'fixed' }}
+        style={{
+          backgroundColor: '#F7F8FA',
+          height: '100%',
+          width: '100%',
+          backgroundAttachment: 'fixed',
+        }}
       >
         <div
           style={{
