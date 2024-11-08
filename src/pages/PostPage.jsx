1a3dd1ac0aeb675a8c2a1b318825bc7e778cb54e
Pasak22 <125808024+Pasak22@users.noreply.github.com>
2024-11-08 20:05:20 +0900

    Feat(#24): 글 작성 post 요청 구현

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -20,6 +20,7 @@ import 'swiper/css/pagination';
 
 // import required modules
 import { FreeMode, Pagination } from 'swiper/modules';
+import { useNavigate } from 'react-router-dom';
 
 const CATEGORY_LIST = [
   '한식',
@@ -40,19 +41,25 @@ export default function PostPage() {
   const [selectedFoodCategory, setSelectedFoodCategory] = useState('한식');
   const [userInputTitle, setUserInputTitle] = useState('');
   const [userInputContent, setUserInputContent] = useState('');
-  const [userInputTotalCount, setUserInputTotalCount] = useState(0);
+  const [userInputTotalCount, setUserInputTotalCount] = useState(2);
+
+  const navigate = useNavigate();
 
 
   // 밥 메이트 선택 후 확인 버튼 클릭 시
   const babConfirmClick = () => {
     let isAnonymous = false;
     if (selectedCategory === '익명 메이트') isAnonymous=true;
-    postBap(userInputTitle, userInputContent, selectedCategory, isAnonymous, userInputTotalCount)
+    postBap(userInputTitle, userInputContent, selectedFoodCategory, isAnonymous, userInputTotalCount)
+    window.alert("등록 완료.");
+    navigate('/');
   };
 
   // 간식 내기 선택 후 확인 버튼 클릭 시
   const snackConfirmClick = () => {
     postGame(userInputTitle, userInputContent, userInputTotalCount);
+    window.alert("등록 완료.");
+    navigate('/');
   };
 
   const handleCategoryClick = (category) => {
@@ -69,6 +76,11 @@ export default function PostPage() {
     return true;
   };
 
+  const checkFilledInput = () => {
+    if (userInputTitle === '' || userInputContent === '') return false;
+    else return true;
+  }
+
   return (
     <div className="root">
       <div
@@ -78,11 +90,11 @@ export default function PostPage() {
           paddingTop: '80px',
         }}
       >
-        {TITLE_CATEGORY_LIST.map((elem, i) => {
+        {TITLE_CATEGORY_LIST.map((elem, index) => {
           return (
             <>
               <CategoryButton
-                key={i}
+                key={index}
                 text={elem}
                 style={{ width: '137px', height: '44px' }}
                 isSelected={selectedCategory === elem}
@@ -92,8 +104,6 @@ export default function PostPage() {
           );
         })}
       </div>
-
-      {!isGamePage() ? (
         <div
           style={{
             marginTop: '25px',
@@ -117,6 +127,7 @@ export default function PostPage() {
               />
             </Form.Group>
           </div>
+          {!isGamePage() ? (
           <div>
             <div className="dm-board-list-wrapper">
               <div>카테고리</div>
@@ -135,7 +146,7 @@ export default function PostPage() {
                       <>
                         <SwiperSlide key={i}>
                           <CategoryButton
-                            key={i}
+                            // key={i}
                             text={elem}
                             style={{
                               width: '107px',
@@ -152,7 +163,7 @@ export default function PostPage() {
                 </Swiper>
               </div>
             </div>
-          </div>
+          </div>) : (<></>)}
           <div>
             <Form.Group
               className="mb-3"
@@ -173,10 +184,15 @@ export default function PostPage() {
           <div>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>총 인원</Form.Label>
-              <Form.Control type="number"
+              <Form.Control type="number" min="2" max="30" value={userInputTotalCount}
               onChange={(e) => {
-                setUserInputTotalCount(e.target.value);
-                console.log(e.target.value);
+                let value = parseInt(e.target.value, 10);
+
+                if (value < 2) value = 2;
+                if (value > 30) value = 30;
+
+                setUserInputTotalCount(value);
+                console.log(value);
               }} />
             </Form.Group>
           </div>
@@ -199,8 +215,12 @@ export default function PostPage() {
                 boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.2)',
               }}
             onClick={()=> {
-              console.log("asd")
-              babConfirmClick();
+              if (checkFilledInput()) {
+                if (selectedCategory === '밥 메이트' || selectedCategory === '익명 메이트') babConfirmClick();
+                else snackConfirmClick();
+              } else {
+                window.alert("제목 및 내용을 모두 입력해주세요.");
+              }
             }}
             
             >
@@ -214,90 +234,6 @@ export default function PostPage() {
             </Button>
           </div>
         </div>
-      ) : (
-        <div
-          style={{
-            marginTop: '25px',
-            fontSize: '15px',
-            fontFamily: 'Jalnan',
-            color: '#000000',
-          }}
-        >
-          <div>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>제목</Form.Label>
-              <Form.Control
-                as="input"
-                type="text"
-                placeholder="장소, 시간 등을 포함해주세요"
-                onChange={(e) => {
-                  setUserInputTitle(e.target.value);
-                  console.log(e.target.value);
-                }}
-              />
-            </Form.Group>
-          </div>
-          <div>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>내용</Form.Label>
-              <Form.Control
-                as="textarea"
-                rows={5}
-                placeholder="장소, 시간 등을 포함해주세요"
-                onChange={(e) => {
-                  setUserInputContent(e.target.value);
-                  console.log(e.target.value);
-                }}
-              />
-            </Form.Group>
-          </div>
-          <div>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>총 인원</Form.Label>
-              <Form.Control type="number" onChange={(e) => {
-                setUserInputTotalCount(e.target.value);
-                console.log(e.target.value);
-              }} />
-            </Form.Group>
-          </div>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-            }}
-          >
-            <Button
-              style={{
-                backgroundColor: '#002DA7',
-                color: '#FFFFFF',
-                border: '1px solid #0B04D9',
-                fontFamily: 'Jalnan',
-                fontSize: '20px',
-                borderRadius: '10px',
-                width: '173px',
-                height: '49px',
-                boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.2)',
-              }}
-              onClick={() => {
-                console.log("클릭");
-                
-                snackConfirmClick();
-              }}
-            >
-              <div
-                style={{
-                  marginTop: '3px',
-                }}
-              >
-                등록
-              </div>
-            </Button>
-          </div>
-        </div>
-      )}
     </div>
   );
 }
